6a3c367aa65ccfea6b9594e32d4fac31f3d743ba
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-07-26 17:27:27 +0400

    added the rest of ecc_lab week 3

--- Brown-matrix/ecc_lab/ecc_lab.py
+++ Brown-matrix/ecc_lab/ecc_lab.py
@@ -1,8 +1,9 @@
 from vec import Vec
-from mat import Mat, matrix_vector_mul
-#from bitutil import noise
+from mat import Mat
+from bitutil import *
 from GF2 import one
-from matutil import listlist2mat
+from matutil import listlist2mat, mat2coldict, coldict2mat
+
 ## Task 1 part 1
 """ Create an instance of Mat representing the generator matrix G. You can use
 the procedure listlist2mat in the matutil module (be sure to import first).
@@ -12,16 +13,16 @@ G = listlist2mat([[one,0,one,one],[one,one,0,one], [0,0,0,one], [one, one, one, 
 
 ## Task 1 part 2
 # Please write your answer as a list. Use one from GF2 and 0 as the elements.
-encoding_1001 = [one, one, one, one, one, one, one]
+encoding_1001 = [0,0,one,one,0,0,one]
 
 
 ## Task 2
 # Express your answer as an instance of the Mat class.
-R = Mat(({'a', 'b', 'c', 'd'}, {0,1,2,3,4,5,6}), {(3, 'd'): 27})
+R = Mat(({0, 1, 2, 3}, {0, 1, 2, 3, 4, 5, 6}), {(0,6):one, (1,5):one, (2,4):one, (3,2):one})
 
 ## Task 3
 # Create an instance of Mat representing the check matrix H.
-H = None
+H = listlist2mat([[0,0,0,one,one,one,one],[0,one,one,0,0,one,one],[one,0,one,0,one,0,one]])
 
 ## Task 4 part 1
 def find_error(e):
@@ -36,14 +37,25 @@ def find_error(e):
         >>> find_error(Vec({0,1,2}, {1:one, 2:one}))
         Vec({0, 1, 2, 3, 4, 5, 6},{2: one})    
     """
-    pass
+    assert e.D == {0,1,2}
+    index = 0
+    if e[0] is one:
+        index += 4
+    if e[1] is one:
+        index += 2
+    if e[2] is one:
+        index += 1
+    d = {}
+    if index > 0:
+        d = { (index-1):one }
+    return Vec(set(range(7)), d)
 
 ## Task 4 part 2
 # Use the Vec class for your answers.
 non_codeword = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one, 6:one})
-error_vector = Vec(..., ...)
-code_word = Vec(..., ...)
-original = ... # R * code_word
+error_vector = find_error(H*non_codeword)
+code_word = non_codeword+error_vector
+original = R*code_word
 
 
 ## Task 5
@@ -56,20 +68,20 @@ def find_error_matrix(S):
         >>> find_error_matrix(S)
         Mat(({0, 1, 2, 3, 4, 5, 6}, {0, 1, 2, 3}), {(1, 2): 0, (3, 2): one, (0, 0): 0, (4, 3): one, (3, 0): 0, (6, 0): 0, (2, 1): 0, (6, 2): 0, (2, 3): 0, (5, 1): one, (4, 2): 0, (1, 0): 0, (0, 3): 0, (4, 0): 0, (0, 1): 0, (3, 3): 0, (4, 1): 0, (6, 1): 0, (3, 1): 0, (1, 1): 0, (6, 3): 0, (2, 0): 0, (5, 0): 0, (2, 2): 0, (1, 3): 0, (5, 3): 0, (5, 2): 0, (0, 2): 0})
     """
-    pass
+    return coldict2mat({ i:find_error(col) for (i,col) in mat2coldict(S).items() })
 
 ## Task 6
 s = "I'm trying to free your mind, Neo. But I can only show you the door. You’re the one that has to walk through it."
-P = None
+P = bits2mat(str2bits(s))
 
 ## Task 7
-C = None
-bits_before = None
-bits_after = None
+C = G*P
+bits_before = len(P.D[0])*len(P.D[1])
+bits_after = 7/4*bits_before
 
 
 ## Ungraded Task
-CTILDE = None
+CTILDE = C+noise(C, 0.02)
 
 ## Task 8
 def correct(A):
@@ -81,4 +93,13 @@ def correct(A):
         >>> correct(A)
         Mat(({0, 1, 2, 3, 4, 5, 6}, {1, 2, 3}), {(0, 1): 0, (1, 2): 0, (3, 2): 0, (1, 3): 0, (3, 3): 0, (5, 2): one, (6, 1): 0, (3, 1): 0, (2, 1): 0, (0, 2): one, (6, 3): one, (4, 2): 0, (6, 2): one, (2, 3): 0, (4, 3): 0, (2, 2): 0, (5, 1): 0, (0, 3): one, (4, 1): 0, (1, 1): 0, (5, 3): one})
     """
-    pass
+    return A+find_error_matrix(H*A)
+
+# Ungraded
+def roundtrip(s, probability):
+    c = G*bits2mat(str2bits(s))
+    e = noise(c, probability)
+    ce = c + e
+    print(s)
+    print(bits2str(mat2bits(R*ce)))
+    print(bits2str(mat2bits(R*correct(ce))))
